88ade7e3b7a5d57ec2ce3844906f207fd89ab452
Rik Janssen <56626554+the-beta-core@users.noreply.github.com>
2020-09-11 14:30:59 +0200

    Update ssel.php

--- ssel.php
+++ ssel.php
@@ -3,7 +3,7 @@
 * Plugin Name: Super Simple Events List
 * Plugin URI: https://betacore.tech/plugins/super-simple-event-list-for-wordpress/
 * Description: Create and customise a simple events (and past events) list. Display them on your page with a shortcode. In the WP admin it has the look and feel of Wordpress. Nothing fancy.
-* Version: 0.8
+* Version: 0.9
 * Author: Rik Janssen 
 * Author URI: https://www.rikjanssen.info/
 * Text Domain: bcssel
